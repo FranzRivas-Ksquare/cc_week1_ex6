a2bf014f2b2d6ba9ff3371e424154444eb5b518b
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-09-23 12:54:47 -0500

    finish activityu 6

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -3,7 +3,9 @@ const { cypher } = require('../exercise');
 test('Testing the cypher exercise', () => { 
     expect(cypher('')).toBeDefined();
     expect(cypher('JRYPBZR GB VGX!')).toBe('WELCOME TO ITK!')
+    expect(cypher('jrypbzr gb vgx!')).toBe('welcome to itk!')
     expect(cypher('PBER PBHEFR 2022')).toBe('CORE COURSE 2022')
+    expect(cypher('Pber Pbhefr 2022')).toBe('Core Course 2022')
     expect(cypher('1 ZRQVHZ CVMMN')).toBe('1 MEDIUM PIZZA')
     expect(cypher('GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.')).toBe('THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.')
  })
--- lab.js
+++ lab.js
@@ -1,21 +1,9 @@
-/* *Instructions*
-    You will solve one of the most famous cypher... Caesar's cypher.
-
-    This is a shift cypher meaning that all the letters shift it's position by a set amount.
-
-    You need to implement a cypher that rotates each later exactly 13 positions (using only the English alphabeth),
-    as input you will get a coded string and you must return the decoded result.
-
-
-*/
-
-/** DO NOT CHANGE THE FUNCTION NAME **/
 const cypher = (encoded) => {
-    
-    /* Only make changes below this comment */
     let arr = [];
+    /* Only make changes below this comment */
     for (letter in encoded) {
         let ascii = encoded.charCodeAt(letter);
+        console.log(`original: ${ascii} = to letter: ${encoded[letter]}`);
         if (ascii >= 65 && ascii <= 90) {
             ascii = ascii + 13;
             if (ascii > 90) {
@@ -28,16 +16,14 @@ const cypher = (encoded) => {
                 ascii = ascii - 26;
             };
         };
+        console.log(`convert: ${ascii} = char: ${String.fromCharCode(ascii)}`);
+        console.log('-----------------------');
+
         arr.push(String.fromCharCode(ascii));
     };
     result = arr.join('');
     return result;
     /* Only make changes above this comment */
-}
-
-
-
-
-
-/** DO NOT CHANGE THE LINE BELOW **/
-module.exports.cypher = cypher;
+};
+console.log(cypher('JRYPBZR GB VGX!'));
+console.log(cypher('jrypbzr gb vgx!'));
